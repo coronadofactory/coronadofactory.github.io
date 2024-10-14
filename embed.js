4414d0ed2ac6904ab484b575b707921e0dd4a4d7
Software factory <factory@coronadoland.com>
2024-10-14 07:39:51 +0200

    First Version

---
+++ embed.js
@@ -0,0 +1,19 @@
+(function(document) {
+  const 
+  embed = document.querySelector("[embed]").getAttribute("embed"), 
+  src = 'https://minerva.coronadofactory.com/'+embed+'.js', 
+  body = document.getElementsByTagName('body')[0], 
+  script = document.createElement('script');
+
+script.setAttribute('src',src)
+
+body.appendChild(script);
+
+setTimeout(function() {
+    if (document.getElementById('loader')) {
+      document.getElementById('loader').remove(); 
+      document.getElementById('iframe').remove(); 
+      document.getElementById('error').style.display='block';
+    }
+  },5000);
+})(document);
